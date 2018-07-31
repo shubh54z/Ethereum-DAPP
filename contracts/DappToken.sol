7b44c3cfaa045b06372405f61c46781bfe4f154a
Terry Kemp <Terry.kemp14@gmail.com>
2018-07-31 13:31:23 -0400

    .

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -30,6 +30,8 @@ contract DappToken {
     );
     
     mapping(address => uint256) public balanceOf;
+    //create allowance
+    mapping(address => mapping(address => uint256)) public allowance;
 
     function DappToken (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -47,11 +49,18 @@ contract DappToken {
     }
 //approve function
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        //allowance
-//approval event
+        allowance[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-
-
+    //transfer from
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
 } 
